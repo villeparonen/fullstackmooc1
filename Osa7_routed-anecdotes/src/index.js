2db5dcc2103239de57c9c1335a990e153bb397f6
Ville Paronen <45661106+villeparonen@users.noreply.github.com>
2020-08-30 11:51:15 +0300

    Add files via upload
    Osa7 - routed anecdotes tehtävät

---
+++ Osa7_routed-anecdotes/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {
+    BrowserRouter as Router,
+    Switch, Route, Link
+  } from "react-router-dom"
+
+ReactDOM.render(
+<Router>
+  <App />
+  </Router>, 
+  document.getElementById('root')
+  )
